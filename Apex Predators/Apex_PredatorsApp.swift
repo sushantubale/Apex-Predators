ac95f707b7e82497df6dd07482a40dbae3d53110
sushant ubale <suubale@cisco.com>
2024-06-16 00:08:29 -0500

    Initial Commit

---
+++ Apex Predators/Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_PredatorsApp.swift
+//  Apex Predators
+//
+//  Created by suubale on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
